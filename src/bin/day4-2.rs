277874e25dce26c4822c21f71f6b61aba6180146
taoky <me@taoky.moe>
2022-12-25 19:46:58 +0800

    day4

---
+++ src/bin/day4-2.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut cnt = 0;
+    loop {
+        let mut input = String::new();
+        let input_size = std::io::stdin().read_line(&mut input).unwrap();
+        if input_size == 0 {
+            break;
+        }
+        let pairs: Vec<_> = input.trim().split(',').collect();
+        assert!(pairs.len() == 2);
+        let p1: Vec<_> = pairs[0].split('-').collect();
+        let p2: Vec<_> = pairs[1].split('-').collect();
+
+        let p1_start: i32 = p1[0].parse().unwrap();
+        let p1_end: i32 = p1[1].parse().unwrap();
+        let p2_start: i32 = p2[0].parse().unwrap();
+        let p2_end: i32 = p2[1].parse().unwrap();
+
+        if p1_end < p2_start || p2_end < p1_start {
+            continue;
+        }
+        cnt += 1;
+    }
+    println!("{}", cnt);
+}
