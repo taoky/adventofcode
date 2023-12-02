88b88c3f9c829686134bad1cd2e782f7933f7722
taoky <me@taoky.moe>
2023-12-02 16:43:00 +0800

    Rewrite Day1 with RIO

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
-import qualified Day1
+import Prelude (IO)
+import Day1 qualified
+import RIO (readFileUtf8)
 
 main :: IO ()
 main = do
-    contents <- readFile "input/day1"
-    -- contents <- readFile "input/day1-2.test"
-    Day1.solve1 contents
-    Day1.solve2 contents
+  contents <- readFileUtf8 "input/day1"
+  -- contents <- readFile "input/day1-2.test"
+  Day1.solve1 contents
+  Day1.solve2 contents
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,15 +1,23 @@
-module Day1 where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day1 (solve1, solve2) where
 
 import Data.Char (isDigit)
-import Data.List (find, isInfixOf)
-import Data.Maybe (maybeToList, fromJust)
--- import Data.Text (Text, pack, replace, unpack)
-import Data.Bifunctor (bimap)
-import qualified Data.Map as Map
+import Data.Either (fromRight)
+import Data.List (find)
+import Data.Map qualified as Map
+import Data.Maybe (fromJust)
+import Data.Text.Read (decimal)
+import RIO.Text (Text)
+import RIO.Text qualified as T
+import RIO.Text.Partial qualified as T'
+import Prelude
 
-filterFirstAndLastNumeric :: String -> String
-filterFirstAndLastNumeric s = maybeToList (find isDigit s) ++ maybeToList (find isDigit (reverse s))
+filterFirstAndLastNumeric :: Text -> Text
+-- filterFirstAndLastNumeric s = maybeToList (find isDigit s) ++ maybeToList (find isDigit (reverse s))
+filterFirstAndLastNumeric s = T.singleton (T'.head (T.filter isDigit s)) `T.append` T.singleton (T'.last (T.filter isDigit s))
 
+wordToDigit :: Map.Map Text Text
 wordToDigit = Map.fromList [("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9")]
 
 -- stringTupleListToTextTupleList :: [(String, String)] -> [(Text, Text)]
@@ -18,46 +26,50 @@ wordToDigit = Map.fromList [("one", "1"), ("two", "2"), ("three", "3"), ("four",
 -- replaceWordWithDigit :: String -> String
 -- replaceWordWithDigit s = unpack (foldl (\acc (old, new) -> replace old new acc) (pack s) (stringTupleListToTextTupleList wordToDigit))
 
-searchSubstring :: String -> [String] -> Maybe String
-searchSubstring s = find (`isInfixOf` s)
+searchSubstring :: Text -> [Text] -> Maybe Text
+-- searchSubstring s = find (`isInfixOf` s)
+searchSubstring s = find (`T.isInfixOf` s)
 
 -- digit, or words in wordToDigit
-findFirstNumeric :: String -> Maybe Int
+findFirstNumeric :: Text -> Maybe Int
 findFirstNumeric s = do
-    let maybeDigit = find isDigit s
-    case maybeDigit of
-        Just digit -> Just (read [digit] :: Int)
-        Nothing -> do
-            case searchSubstring s (Map.keys wordToDigit) of
-                Just key -> Just (read (fromJust (Map.lookup key wordToDigit)) :: Int)
-                Nothing -> Nothing
+  let maybeDigit = find isDigit (T.unpack s)
+  case maybeDigit of
+    Just digit -> Just (read [digit] :: Int)
+    Nothing -> do
+      case searchSubstring s (Map.keys wordToDigit) of
+        -- Just key -> Just (read (fromJust (Map.lookup key wordToDigit)) :: Int)
+        Just key -> Just (fst (fromRight (0, T.empty) (decimal (fromJust (Map.lookup key wordToDigit)))))
+        Nothing -> Nothing
 
-getFirstLineNumber :: String -> Bool -> Int
-getFirstLineNumber s r = go s r 1
-    where
-        go :: String -> Bool -> Int -> Int
-        go s isReverse lineNumber = do
-            -- starts from 1, 2, 3, ... characters
-            let (first, rest) = splitAt (if isReverse then length s - lineNumber else lineNumber) s
-            case findFirstNumeric (if isReverse then rest else first) of
-                Just number -> number
-                Nothing -> go s isReverse (lineNumber + 1)
+getFirstLineNumber :: Text -> Bool -> Int
+getFirstLineNumber s r = go r 1
+  where
+    go :: Bool -> Int -> Int
+    go isReverse lineNumber = do
+      -- starts from 1, 2, 3, ... characters
+      let (first, rest) = T.splitAt (if isReverse then T.length s - lineNumber else lineNumber) s
+      case findFirstNumeric (if isReverse then rest else first) of
+        Just number -> number
+        Nothing -> go isReverse (lineNumber + 1)
 
-getLineNumber :: String -> Int
+getLineNumber :: Text -> Int
 getLineNumber s = 10 * getFirstLineNumber s False + getFirstLineNumber s True
 
-stringToInt :: String -> Int
-stringToInt str = read str :: Int
+stringToInt :: Text -> Int
+-- stringToInt str = read (T.unpack str) :: Int
+stringToInt str = fst (fromRight (0, T.empty) (decimal str))
 
-solve1 :: String -> IO ()
+solve1 :: Text -> IO ()
 solve1 input =
-  let linesOfInput = lines input
+  let linesOfInput = T.lines input
       filteredLines = map (stringToInt . filterFirstAndLastNumeric) linesOfInput
    in print $ sum filteredLines
 
-solve2 :: String -> IO ()
+solve2 :: Text -> IO ()
 solve2 input =
-  let linesOfInput = lines input
+  let linesOfInput = T.lines input
       filteredLines = map getLineNumber linesOfInput
    in print $ sum filteredLines
-    -- in print filteredLines
+
+-- in print filteredLines
