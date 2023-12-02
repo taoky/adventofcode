241a59b7ea08e22d751f7370a2c7db6471637447
taoky <me@taoky.moe>
2023-12-02 18:27:30 +0800

    Add options-applicative for main (choose day to run)

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,59 @@
+{-# OPTIONS -Wno-unused-top-binds #-}
 module Main (main) where
 
-import Prelude (IO)
 import Day1 qualified
+import Options.Applicative
 import RIO (readFileUtf8)
+import RIO.Text (Text)
+import Prelude
+
+newtype Options = Options
+  { day :: Maybe Int
+  }
+
+options :: Parser Options
+options =
+  Options
+    <$> optional
+      ( option
+          auto
+          ( long "day"
+              <> short 'd'
+              <> metavar "DAY"
+              <> help "Day to run"
+          )
+      )
 
 main :: IO ()
-main = do
-  contents <- readFileUtf8 "input/day1"
-  -- contents <- readFile "input/day1-2.test"
-  Day1.solve1 contents
-  Day1.solve2 contents
+main = solutions =<< execParser opts
+  where
+    opts =
+      info
+        (options <**> helper)
+        ( fullDesc
+            <> progDesc "Run solutions"
+            <> header "2023-hs"
+        )
+
+dayToModule :: [(Int, [Text -> IO ()])]
+dayToModule = [(1, [Day1.solve1, Day1.solve2])]
+
+solutions :: Options -> IO ()
+solutions (Options day) = do
+  case day of
+    Just d -> do
+      let m = lookup d dayToModule
+      case m of
+        Just x -> do
+          contents <- readFileUtf8 ("input/day" ++ show d)
+          mapM_ (\f -> f contents) x
+        Nothing -> putStrLn "No such day"
+    Nothing -> do
+      -- Run all
+      putStrLn "Run all"
+      mapM_
+        ( \(d, x) -> do
+            contents <- readFileUtf8 ("input/day" ++ show d)
+            mapM_ (\f -> f contents) x
+        )
+        dayToModule
