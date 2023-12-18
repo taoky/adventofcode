5c1c659d147e56fe044a8eb413e8c4ab70865cee
taoky <me@taoky.moe>
2023-12-19 02:14:11 +0800

    Day16: try Data.Vector.Unboxed
    (Does not help though)

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,16 +1,31 @@
 module Day16 (solve1, solve2) where
 
 import Data.Hashable (hashWithSalt)
-import Data.Vector qualified as V
+import Data.Vector.Unboxed qualified as VU
 import RIO
 import RIO.HashSet qualified as HS
-import RIO.List.Partial (maximum)
+import RIO.List.Partial (maximum, head)
 import RIO.Text qualified as T
 import Prelude (print)
 
 data Direction = Up | Dow | Lef | Righ deriving (Show, Eq)
 
-type Matrix = V.Vector (V.Vector Char)
+data Matrix = Matrix
+  { _matrix :: !(VU.Vector Char),
+    _width :: !Int,
+    _height :: !Int
+  }
+  deriving (Show, Eq)
+
+makeMatrix :: [[Char]] -> Matrix
+makeMatrix xs =
+  let width = length $ head xs
+      height = length xs
+      matrix = VU.fromList $ concat xs
+   in Matrix matrix width height
+
+getMatrixElement :: Matrix -> (Int, Int) -> Char
+getMatrixElement (Matrix matrix width _) (x, y) = matrix VU.! (x * width + y)
 
 instance Hashable Direction where
   hashWithSalt salt Up = hashWithSalt salt (0 :: Int)
@@ -19,7 +34,7 @@ instance Hashable Direction where
   hashWithSalt salt Righ = hashWithSalt salt (3 :: Int)
 
 parse :: Text -> Matrix
-parse input = V.fromList $ map (V.fromList . T.unpack) (T.lines input)
+parse input = makeMatrix $ map T.unpack (T.lines input)
 
 step :: (Int, Int) -> Direction -> (Int, Int)
 step (x, y) Up = (x - 1, y)
@@ -29,14 +44,14 @@ step (x, y) Righ = (x, y + 1)
 
 isInBound :: (Int, Int) -> Matrix -> Bool
 isInBound (x, y) map' =
-  let maxX = length map'
-      maxY = length $ V.head map'
+  let maxX = _height map'
+      maxY = _width map'
    in x >= 0 && x < maxX && y >= 0 && y < maxY
 
 go :: (Int, Int) -> Direction -> Matrix -> HashSet ((Int, Int), Direction) -> HashSet ((Int, Int), Direction)
 go (x, y) dir map' hs =
   let newHs = HS.insert ((x, y), dir) hs
-      current = map' V.! x V.! y
+      current = getMatrixElement map' (x, y)
       nextDirs = case current of
         '.' -> [dir]
         '/' ->
@@ -82,8 +97,8 @@ solve2 :: Text -> IO ()
 solve2 input =
   let map' = parse input
       choices =
-        map (\x -> ((0, x), Dow)) [0 .. length (V.head map') - 1]
-          ++ map (\x -> ((length map' - 1, x), Up)) [0 .. length (V.head map') - 1]
-          ++ map (\x -> ((x, 0), Righ)) [0 .. length map' - 1]
-          ++ map (\x -> ((x, length (V.head map') - 1), Lef)) [0 .. length map' - 1]
+        map (\x -> ((0, x), Dow)) [0 .. _width map' - 1]
+          ++ map (\x -> ((_height map' - 1, x), Up)) [0 .. _width map' - 1]
+          ++ map (\x -> ((x, 0), Righ)) [0 .. _height map' - 1]
+          ++ map (\x -> ((x, _width map' - 1), Lef)) [0 .. _height map' - 1]
    in print $ maximum $ map (uncurry (getResult map')) choices
