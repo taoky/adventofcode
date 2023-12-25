ecfc2112adb963d689f64cc8b60dca959c0464f1
taoky <me@taoky.moe>
2023-12-26 04:42:01 +0800

    Day23: finally work it out!

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,12 +1,15 @@
 module Day23 (solve1, solve2) where
 
 import Data.Hashable (hashWithSalt)
-import Data.MemoUgly qualified as Memo
+import Data.Sequence qualified as Seq
 import Data.Vector.Unboxed qualified as VU
 import Data.Vector.Unboxed.Deriving (derivingUnbox)
 import RIO
+import RIO.HashMap qualified as HM
+import RIO.HashMap.Partial qualified as HM'
 import RIO.HashSet qualified as HS
 import RIO.List.Partial (head)
+import RIO.List qualified as L
 import RIO.Text qualified as T
 import Utils
 import Prelude (print)
@@ -68,6 +71,19 @@ towards Dow (x, y) = (x + 1, y)
 towards Lef (x, y) = (x, y - 1)
 towards Righ (x, y) = (x, y + 1)
 
+isFeasible :: Matrix -> (Int, Int) -> Bool
+isFeasible matrix (x, y) = x >= 0 && y >= 0 && x < _height matrix && y < _width matrix && getMatrixElement matrix (x, y) /= Forest
+
+getNearby :: (Int, Int) -> Matrix -> [(Int, Int)]
+getNearby (x, y) matrix =
+  let dirs = [Up, Dow, Lef, Righ]
+      goto = map (\i -> towards i (x, y)) dirs
+   in filter (isFeasible matrix) goto
+
+isJunction :: (Int, Int) -> Matrix -> Bool
+isJunction (x, y) matrix = length (getNearby (x, y) matrix) > 2 || x == 0 || x == _height matrix - 1
+
+-- Returns: (x, y), direction, steps (weight)
 towardsUntilJunction :: Direction -> (Int, Int) -> Matrix -> Int -> Maybe ((Int, Int), Direction, Int)
 towardsUntilJunction dir (x, y) matrix acc =
   let dirs' =
@@ -76,18 +92,11 @@ towardsUntilJunction dir (x, y) matrix acc =
           Dow -> [Lef, Righ]
           Lef -> [Up, Dow]
           Righ -> [Up, Dow]
-      goto_ = filter (\(_, j) -> isFeasible j) $ map (\i -> (i, towards i (x, y))) dirs'
+      goto_ = filter (\(_, j) -> isFeasible matrix j) $ map (\i -> (i, towards i (x, y))) dirs'
       (dir_, (x_, y_)) = head goto_
-      -- (x', y') = towards dir (x, y)
-      -- compute junctions
-      dirs = [Up, Dow, Lef, Righ]
-      goto (x', y') = map (\i -> (i, towards i (x', y'))) dirs
-      isFeasible (x'', y'') = x'' >= 0 && y'' >= 0 && x'' < _height matrix && y'' < _width matrix && getMatrixElement matrix (x'', y'') /= Forest
-      goto' (x', y') = filter (\(_, i) -> isFeasible i) $ goto (x', y')
-      isNextJunction (x', y') = length (goto' (x', y')) > 2 || x' == 0 || x' == _height matrix - 1
    in case length goto_ of
         0 -> Nothing
-        _ -> if isNextJunction (x_, y_) then Just ((x_, y_), dir_, acc + 1) else towardsUntilJunction dir_ (x_, y_) matrix (acc + 1)
+        _ -> if isJunction (x_, y_) matrix then Just ((x_, y_), dir_, acc + 1) else towardsUntilJunction dir_ (x_, y_) matrix (acc + 1)
 
 parse :: Text -> Matrix
 parse input =
@@ -126,8 +135,7 @@ dfs matrix (startx, starty) ignoreSlope =
                 _ -> [Up, Dow, Lef, Righ]
               else [Up, Dow, Lef, Righ]
           goto = map (`towards` (x, y)) dirs
-          isFeasible (x', y') = x' >= 0 && y' >= 0 && x' < _height matrix && y' < _width matrix && getMatrixElement matrix (x', y') /= Forest
-          goto' = filter (\i -> not $ HS.member i hs) $ filter isFeasible goto
+          goto' = filter (\i -> not $ HS.member i hs) $ filter (isFeasible matrix) goto
           hs' = HS.insert (x, y) hs
        in if x == _height matrix - 1
             then acc
@@ -144,24 +152,45 @@ dfs matrix (startx, starty) ignoreSlope =
                   0
                   goto'
 
--- new dfs with towardsUntilJunction
-dfs' :: Matrix -> (Int, Int) -> Int
-dfs' matrix (startx, starty) =
-  go (startx, starty) Dow HS.empty 0
+type Graph = HashMap (Int, Int) [((Int, Int), Int)]
+
+makeJunctionGraph :: (Int, Int) -> Matrix -> Graph
+makeJunctionGraph start matrix =
+  let bfs :: Seq.Seq ((Int, Int), Direction) -> HS.HashSet (Int, Int) -> Graph -> (HS.HashSet (Int, Int), Graph)
+      bfs q h g =
+        case Seq.viewl q of
+          Seq.EmptyL -> (h, g)
+          ((x, y), dir) Seq.:< xs ->
+            let res = towardsUntilJunction dir (x, y) matrix 0
+                h' = HS.insert (x, y) h
+                nextDirs d = case d of
+                  Up -> [Lef, Righ, Up]
+                  Dow -> [Lef, Righ, Dow]
+                  Lef -> [Up, Dow, Lef]
+                  Righ -> [Up, Dow, Righ]
+             in case res of
+                  Nothing -> bfs xs h' g
+                  Just ((x', y'), dir', weight) ->
+                    if HS.member (x', y') h'
+                      then bfs xs h' g
+                      else
+                        let g' = HM.insertWith (++) (x', y') [((x, y), weight)] $ HM.insertWith (++) (x, y) [((x', y'), weight)] g
+                            g'' = HM.insert (x', y') (L.nub $ g' HM'.! (x', y')) $ HM.insert (x, y) (L.nub $ g' HM'.! (x, y)) g'
+                            q' = xs <> Seq.fromList (map ((x', y'),) (nextDirs dir'))
+                         in bfs q' h' g''
+   in snd $ bfs (Seq.singleton (start, Dow)) HS.empty HM.empty
+
+-- new dfs with junction graph
+dfs' :: Graph -> (Int, Int) -> (Int -> Bool) -> Int
+dfs' graph start isEnd =
+  go start HS.empty 0
   where
-    towardsUntilJunctionMemo :: Direction -> (Int, Int) -> Maybe ((Int, Int), Direction, Int)
-    towardsUntilJunctionMemo = Memo.memo $ \dir (x', y') -> towardsUntilJunction dir (x', y') matrix 0
-    go :: (Int, Int) -> Direction -> HS.HashSet (Int, Int) -> Int -> Int
-    go (x, y) d hs acc =
-      let dirs = case d of
-            Up -> [Lef, Righ, Up]
-            Dow -> [Lef, Righ, Dow]
-            Lef -> [Up, Dow, Lef]
-            Righ -> [Up, Dow, Righ]
-          goto = mapMaybe (\i -> towardsUntilJunctionMemo i (x, y)) dirs
-          goto' = filter (\i -> not $ HS.member (fst3 i) hs) goto
+    go :: (Int, Int) -> HS.HashSet (Int, Int) -> Int -> Int
+    go (x, y) hs acc =
+      let goto = HM.lookupDefault [] (x, y) graph
+          goto' = filter (\i -> not $ HS.member (fst i) hs) goto
           hs' = HS.insert (x, y) hs
-       in if x == _height matrix - 1
+       in if isEnd x
             then acc
             else case goto' of
               -- dead end, this shall not be counted in
@@ -169,7 +198,7 @@ dfs' matrix (startx, starty) =
               _ ->
                 foldl'
                   ( \acc' i ->
-                      let res = go (fst3 i) (snd3 i) hs' (acc + thd3 i)
+                      let res = go (fst i) hs' (acc + snd i)
                           maxAcc = if res == -1 then acc' else max acc' res
                        in maxAcc
                   )
@@ -188,4 +217,5 @@ solve2 input =
   let matrix = parse input
       start_ = [(0, y) | y <- [0 .. _width matrix - 1], getMatrixElement matrix (0, y) /= Forest]
       start = head $ assert' (length start_ == 1) start_
-   in print $ dfs' matrix start
+      junctionGraph = makeJunctionGraph start matrix
+   in print $ dfs' junctionGraph start (\x -> x == _height matrix - 1)
